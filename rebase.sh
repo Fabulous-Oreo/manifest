787593962de2d185fce9e7530dd63e5ed0af1f0d
Kshitij Gupta <kshitijgm@gmail.com>
2017-11-02 16:38:28 +0000

    manifest: initial commit
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

---
+++ rebase.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Copyright 2017 Kshitij Gupta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+function patch()
+{
+  # ==================================================
+  # Repos synced from local_manifest
+  whitelist=(
+    manifest
+    device_oneplus_bacon
+    device_oppo_common
+    device_sony_taoshan
+    external_sony_boringssl-compat
+    device_qcom_sepolicy
+    hardware_qcom_camera
+    kernel_oneplus_msm8974
+    kernel_sony_msm8930
+    packages_apps_Snap-bacon
+    vendor_oneplus
+    vendor_sony
+  )
+  # ==================================================
+  # Repo root
+  
+  if [ -z "$(type -t croot)" ]; then
+    while [ ! -e './build/envsetup.sh' ]; do
+      cd ../;
+    done;
+  
+    source ./build/envsetup.sh;
+  
+  fi;
+  croot;
+  # ==================================================
+  # Rebase
+  for repo in $(curl -s https:/api.github.com/orgs/Fabulous-Oreo/repos\?per_page\=200 | grep html_url | awk 'NR%2 == 0' | cut -d ':' -f 2-3 | tr -d '",'); do
+  {
+    for clone_repo in ${whitelist[@]}; do
+    { 
+      if [ "$(echo $repo | cut -d '/' -f 5)" = "$clone_repo" ]
+      then
+        echo ""; 
+        echo -e "\e[0;36mIgnoring whitelisted repo! ($(echo $repo | cut -d '/' -f 5))\e[0m";
+        echo "";
+        continue 2
+      fi
+    }
+    done; 
+    echo "";
+    echo -e "\e[1;32mRebasing $(echo $repo | cut -d '/' -f 5)...\e[0m";
+    cd $(echo $repo | cut -d '/' -f 5 | sed 's/_/\//g');
+    git fetch $repo oreo;
+    git rebase FETCH_HEAD;
+    croot;
+  }
+  done;
+  # ==================================================
+  unset whitelist;
+  # ==================================================
+}
