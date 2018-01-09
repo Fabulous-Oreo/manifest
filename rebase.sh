b947c806ed1ac736e8657d62121fe19cc53cdb3b
Kshitij Gupta <kshitijgm@gmail.com>
2018-01-09 16:31:39 +0530

    rebase: Set branch name separately
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- rebase.sh
+++ rebase.sh
@@ -17,6 +17,7 @@
 
 function patch()
 {
+  branch=oreo-mr1
   # ==================================================
   # Repos synced from local_manifest
   whitelist=(
@@ -74,12 +75,13 @@ function patch()
     echo "";
     echo -e "\e[1;32mRebasing $(echo $repo | cut -d '/' -f 5)...\e[0m";
     cd $(echo $repo | cut -d '/' -f 5 | sed 's/_/\//g');
-    git fetch $repo oreo;
+    git fetch $repo $branch;
     git rebase FETCH_HEAD;
     croot;
   }
   done;
   # ==================================================
   unset whitelist;
+  unset branch;
   # ==================================================
 }
